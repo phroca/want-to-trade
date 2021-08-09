2dd338572309d2acfc865f7d77b929974223de44
ROCA Philippe <roca.philippe63@gmail.com>
2021-08-09 02:59:58 +0200

    update to angular 11

--- src/app/search/shoe-detail/shoe-detail.page.spec.ts
+++ src/app/search/shoe-detail/shoe-detail.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ShoeDetailPage } from './shoe-detail.page';
@@ -7,7 +7,7 @@ describe('ShoeDetailPage', () => {
   let component: ShoeDetailPage;
   let fixture: ComponentFixture<ShoeDetailPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShoeDetailPage ],
       imports: [IonicModule.forRoot()]
--- src/app/shoe-container/shoe-container.component.spec.ts
+++ src/app/shoe-container/shoe-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ShoeContainerComponent } from './shoe-container.component';
@@ -7,7 +7,7 @@ describe('ShoeContainerComponent', () => {
   let component: ShoeContainerComponent;
   let fixture: ComponentFixture<ShoeContainerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShoeContainerComponent ],
       imports: [IonicModule.forRoot()]
